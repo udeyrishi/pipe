165892f51f3d2022a7b8cedfeab331be21f36149
Udey Rishi <udey.rishi@gmail.com>
2018-05-17 02:56:02 -0400

    Add a basic pipeline test

---
+++ src/test/kotlin/com/udeyrishi/pipe/PipelineTest.kt
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2018 Udey Rishi. All rights reserved.
+ */
+package com.udeyrishi.pipe
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@Suppress("EXPERIMENTAL_FEATURE_WARNING")
+@RunWith(JUnit4::class)
+class PipelineTest {
+    @Test
+    fun works() {
+        lateinit var barrier: Barrier<Int>
+        lateinit var aggregator: Aggregator<Pipeline.Passenger<Int>>
+        lateinit var aggregated: List<Int>
+
+        var barrierReachedCount = 0
+
+        val pipeline = buildPipeline<Int>(ordered = true) {
+            addStep("step 1") {
+                it + 1
+            }
+
+            addStep("Step 2") {
+                synchronized(this@PipelineTest) {
+                    barrierReachedCount++
+                }
+                it + 2
+            }
+
+            barrier = addBarrier()
+
+            addStep("Step 3") {
+                it + 3
+            }
+
+            aggregator = addAggregator(capacity = 1000000) {
+                aggregated = it
+                it.map { it + 1 }
+            }
+
+            addStep("Step 4") {
+                it + 4
+            }
+        }
+
+        val orchestrators = mutableListOf(
+                pipeline.push(0),
+                pipeline.push(1),
+                pipeline.push(2)
+        )
+
+        orchestrators.forEach {
+            it.start()
+        }
+
+        while (barrierReachedCount < 3) {
+            Thread.sleep(100)
+        }
+
+        orchestrators.forEach {
+            assertTrue(it.state is State.Running.Attempting)
+        }
+
+        // Everyone is waiting at the barrier. Now that we know the count, we can safely update the aggregator capacity, and then lift the barrier.
+        barrier.lift()
+        aggregator.capacity = 3
+
+        while (orchestrators.any { it.state !is State.Terminal }) {
+            Thread.sleep(100)
+        }
+
+        // Everyone is done
+
+        orchestrators.forEach {
+            assertTrue(it.state is State.Terminal.Success)
+        }
+
+        orchestrators.forEachIndexed { index, orchestrator ->
+            assertEquals(index + 1 + 2 + 3 + 1 + 4, orchestrator.result?.data)
+        }
+
+        assertEquals(listOf(6, 7, 8), aggregated)
+    }
+}
