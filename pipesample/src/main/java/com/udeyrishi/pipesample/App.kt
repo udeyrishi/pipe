a696b306900fb44d99125cff2d80b80cf106e0b2
Udey Rishi <udey.rishi@gmail.com>
2018-09-02 17:13:10 -0400

    Refactor sample app
    - Repo is maintained at app level

---
+++ pipesample/src/main/java/com/udeyrishi/pipesample/App.kt
@@ -0,0 +1,33 @@
+package com.udeyrishi.pipesample
+
+import android.app.Application
+import com.udeyrishi.pipe.Job
+import com.udeyrishi.pipe.repository.InMemoryRepository
+import com.udeyrishi.pipe.repository.MutableRepository
+import com.udeyrishi.pipe.util.AndroidLogger
+
+class App : Application() {
+    companion object {
+        lateinit var INSTANCE: App
+            private set
+    }
+
+    val jobsRepo: MutableRepository<Job<ImagePipelineMember>> = InMemoryRepository()
+    val logger = AndroidLogger("Pipe")
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+    }
+
+    override fun onTerminate() {
+        jobsRepo
+                .getMatching { true }
+                .map { it.identifiableObject }
+                .forEach { it.interrupt() }
+
+        jobsRepo.removeIf { true }
+
+        super.onTerminate()
+    }
+}
--- pipesample/src/main/java/com/udeyrishi/pipesample/MainActivity.kt
+++ pipesample/src/main/java/com/udeyrishi/pipesample/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.udeyrishi.pipesample
 
 import android.arch.lifecycle.Observer
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
 import com.udeyrishi.pipe.Job
-import com.udeyrishi.pipe.Pipeline
 import com.udeyrishi.pipe.State
-import com.udeyrishi.pipe.buildPipeline
-import com.udeyrishi.pipe.repository.InMemoryRepository
 import com.udeyrishi.pipe.util.AndroidLogger
 import kotlinx.android.synthetic.main.activity_main.root
 
@@ -18,104 +14,62 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val JOB_TAG = "IMAGE_TRANSFORM_JOBS"
         private const val LOG_TAG = "Pipe"
-        private const val SCALED_IMAGE_SIZE = 400
     }
-
-    private val imageUrls = listOf(
-            "http://www.yevol.com/en/visualstudio/resources/horizon.jpg",
-            "http://i1.wp.com/ivereadthis.com/wp-content/uploads/2018/03/IMG_0340.jpg",
-            "http://i2.wp.com/debsrandomwritings.com/wp-content/uploads/2015/09/Ugg-with-stitches1.jpg",
-            "http://5v9xs33wvow7hck7-zippykid.netdna-ssl.com/wp-content/uploads/2011/05/Cat-Urine-300x293.jpg",
-            "http://g77v3827gg2notadhhw9pew7-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/what-to-do-cat-seizure_canna-pet-e1490730066628-1024x681.jpg"
-    )
-
-    private val repository = InMemoryRepository<Job<ImagePipelineMember>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onResume() {
-        super.onResume()
-        val pipeline = makePipeline(repository)
+        val imageUrls = listOf(
+                "http://www.yevol.com/en/visualstudio/resources/horizon.jpg",
+                "http://i1.wp.com/ivereadthis.com/wp-content/uploads/2018/03/IMG_0340.jpg",
+                "http://i2.wp.com/debsrandomwritings.com/wp-content/uploads/2015/09/Ugg-with-stitches1.jpg",
+                "http://5v9xs33wvow7hck7-zippykid.netdna-ssl.com/wp-content/uploads/2011/05/Cat-Urine-300x293.jpg",
+                "http://g77v3827gg2notadhhw9pew7-wpengine.netdna-ssl.com/wp-content/uploads/2017/03/what-to-do-cat-seizure_canna-pet-e1490730066628-1024x681.jpg"
+        )
 
-        pipeline.aggregators.forEach {
-            it.capacity = imageUrls.size
-        }
-
-        val jobs = imageUrls.mapIndexed { index, url ->
-            pipeline.push(ImagePipelineMember(index = index, url = url), tag = JOB_TAG)
+        val jobs = if (App.INSTANCE.jobsRepo[JOB_TAG].isEmpty()) {
+            createImageJobs(imageUrls)
+        } else {
+            App.INSTANCE.jobsRepo[JOB_TAG].map { it.identifiableObject }
         }
 
         jobs.forEachIndexed { index, job ->
             job.state.observe(this, Observer { state ->
-                when (state) {
-                    is State.Running, State.Scheduled -> {
-                        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_arrow_downward_black_24dp)
-                        (root.getChildAt(index) as ImageView).setImageDrawable(drawable)
-                    }
-                    is State.Terminal.Success -> {
-                        (root.getChildAt(index) as ImageView).setImageBitmap(job.result?.getBitmap())
-                    }
-                    is State.Terminal.Failure -> {
-                        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_error_black_24dp)
-                        (root.getChildAt(index) as ImageView).setImageDrawable(drawable)
-                    }
-                }
+                onStateChanged(state, index, job)
             })
-        }
 
-        jobs.forEach {
-            it.logger = AndroidLogger(logTag = LOG_TAG)
-            it.start()
+            job.logger = App.INSTANCE.logger
+            job.start()
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        repository[JOB_TAG].forEach {
-            it.identifiableObject.interrupt()
-        }
-
-        repository.removeIf {
-            true
-        }
-    }
-
-    private fun makePipeline(repository: InMemoryRepository<Job<ImagePipelineMember>>): Pipeline<ImagePipelineMember> {
-        return buildPipeline(repository) {
-            addStep("download", attempts = 4) {
-                ImagePipelineMember(index = it.index, image = downloadImage(it.getUrl()))
+    private fun onStateChanged(state: State?, index: Int, job: Job<ImagePipelineMember>) {
+        when (state) {
+            is State.Running, State.Scheduled -> {
+                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_arrow_downward_black_24dp)
+                (root.getChildAt(index) as ImageView).setImageDrawable(drawable)
             }
-
-            addStep("rotate") {
-                ImagePipelineMember(index = it.index, image = rotateBitmap(it.getBitmap(), 90.0f))
+            is State.Terminal.Success -> {
+                (root.getChildAt(index) as ImageView).setImageBitmap(job.result?.getBitmap())
             }
-
-            addStep("scale") {
-                ImagePipelineMember(index = it.index, image = scale(it.getBitmap(), SCALED_IMAGE_SIZE, SCALED_IMAGE_SIZE))
-            }
-
-            addAggregator("overdraw", capacity = Int.MAX_VALUE) { allMembers ->
-                allMembers.map { member ->
-                    val siblingIndex = member.index + (if (member.index % 2 == 0) 1 else -1)
-                    val resultingImage = if (siblingIndex < allMembers.size) {
-                        overdraw(member.getBitmap(), allMembers[siblingIndex].getBitmap())
-                    } else {
-                        member.getBitmap()
-                    }
-                    ImagePipelineMember(index = member.index, image = resultingImage)
-                }
+            is State.Terminal.Failure -> {
+                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_error_black_24dp)
+                (root.getChildAt(index) as ImageView).setImageDrawable(drawable)
             }
         }
     }
-}
 
-private class ImagePipelineMember(val index: Int, private val url: String? = null, private val image: Bitmap? = null) {
-    fun getBitmap(): Bitmap
-        = image ?: throw IllegalStateException("image must've been read.")
+    private fun createImageJobs(imageUrls: List<String>): List<Job<ImagePipelineMember>> {
+        val pipeline = makePipeline(App.INSTANCE.jobsRepo)
+        pipeline.aggregators.forEach {
+            it.capacity = imageUrls.size
+        }
 
-    fun getUrl(): String
-        = url ?: throw IllegalStateException("URL must've been non-null.")
+        return imageUrls.mapIndexed { index, url ->
+            pipeline.push(ImagePipelineMember(index = index, url = url), tag = JOB_TAG).also {
+                it.logger = AndroidLogger(LOG_TAG)
+            }
+        }
+    }
 }
