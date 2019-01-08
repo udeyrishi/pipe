521f3b124663ea7e0bd9a770de5b418fe6c85ab2
Udey Rishi <udey.rishi@gmail.com>
2019-01-08 01:06:19 -0500

    Improved the example to persist jobs on rotation

--- pipesample/src/main/java/com/udeyrishi/pipesample/MainActivity.kt
+++ pipesample/src/main/java/com/udeyrishi/pipesample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.udeyrishi.pipesample
 
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -19,36 +20,40 @@ import kotlinx.android.synthetic.main.activity_main.root_view as rootView
 
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-
-        rootView.setOnRefreshListener {
-            App.jobsRepo[JOB_TAG].forEach { (job, _, _) ->
-                job.state.removeObservers(this)
-                job.interrupt()
-            }
-
-            App.jobsRepo.removeIf {
-                it.tag == JOB_TAG
-            }
-
-            val pipeline = runPipeline()
-            pipeline.manualBarriers[0].liftWhenHasInternet(this)
-            pipeline.manualBarriers[1].lift()
-        }
-
-        val pipeline = runPipeline()
-        pipeline.manualBarriers[0].liftWhenHasInternet(this)
+        rootView.setOnRefreshListener(::onRefresh)
         rootView.findViewById<Button>(R.id.start_button).setOnClickListener {
-            it.visibility = View.GONE
-            rootView.isRefreshing = true
-            pipeline.manualBarriers[1].lift()
+            startJobs()
         }
+
+        App.jobsRepo[JOB_TAG]
+                .map {
+                    @Suppress("UNCHECKED_CAST")
+                    it.identifiableObject as Job<ImagePipelineMember>
+                }
+                .setupStateChangeListeners()
+        // If App.jobsRepo[JOB_TAG] is empty, jobs haven't been started yet, and the start_button is visible.
+        // Clicking it will start the jobs.
+        // Else, any state notification will hide the start button, and update the images + spinner appropriately.
     }
 
-    private fun runPipeline(): Pipeline<ImagePipelineMember> {
+    private fun onRefresh() {
+        App.jobsRepo[JOB_TAG].forEach { (job, _, _) ->
+            job.state.removeObservers(this)
+            job.interrupt()
+        }
+
+        App.jobsRepo.removeIf {
+            it.tag == JOB_TAG
+        }
+
+        startJobs()
+    }
+
+    private fun startJobs() {
         val imageUrls = listOf(
                 "http://pre00.deviantart.net/ac5d/th/pre/f/2013/020/1/d/cat_stock_3_by_manonvr-d5s437u.jpg",
                 "http://i1.wp.com/ivereadthis.com/wp-content/uploads/2018/03/IMG_0340.jpg",
@@ -59,32 +64,32 @@ class MainActivity : AppCompatActivity() {
         val pipeline = makePipeline()
         val jobs = pipeline.createImageJobs(imageUrls)
 
-        jobs.forEachIndexed { index, job ->
-            job.state.observe(this, Observer {
+        jobs.setupStateChangeListeners()
+
+        pipeline.manualBarriers[0].liftWhenHasInternet(this)
+        pipeline.manualBarriers[1].lift()
+    }
+
+    private fun List<Job<ImagePipelineMember>>.setupStateChangeListeners() {
+        forEachIndexed { index, job ->
+            job.state.observe(this@MainActivity, Observer {
                 onStateChanged(it, index, job)
             })
         }
-
-        return pipeline
     }
 
-    private fun onStateChanged(state: State?, index: Int, job: Job<ImagePipelineMember>) {
-        val imageView = (imageContainer.getChildAt(index + 1) as ImageView)
-        when (state) {
-            is State.Running, State.Scheduled -> {
-                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_arrow_downward_black_24dp)
-                imageView.setImageDrawable(drawable)
-            }
-            is State.Terminal.Success -> {
-                rootView.isRefreshing = false
-                imageView.setImageBitmap(job.result?.image)
-            }
-            is State.Terminal.Failure -> {
-                rootView.isRefreshing = false
-                val drawable = ContextCompat.getDrawable(this, R.drawable.ic_error_black_24dp)
-                imageView.setImageDrawable(drawable)
-            }
+    private fun onStateChanged(state: State, index: Int, job: Job<ImagePipelineMember>) {
+        val drawable = when (state) {
+            is State.Running, State.Scheduled -> null
+            is State.Terminal.Success -> BitmapDrawable(resources, job.result?.image)
+            is State.Terminal.Failure -> ContextCompat.getDrawable(this, R.drawable.ic_error_black_24dp)
         }
+
+        (imageContainer.getChildAt(index + 1) as ImageView).setImageDrawable(drawable)
+        rootView.isRefreshing = drawable == null
+        // If we're getting state changes, means that the jobs are active/completed. i.e., the start
+        // button would've been clicked by now.
+        rootView.findViewById<Button>(R.id.start_button).visibility = View.GONE
     }
 
     private fun Pipeline<ImagePipelineMember>.createImageJobs(imageUrls: List<String>): List<Job<ImagePipelineMember>> {
