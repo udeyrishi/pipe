c00c416dda4a10ea677c1d199f11c30a655f8768
Udey Rishi <udey.rishi@gmail.com>
2019-01-08 23:49:08 -0500

    Simplified TestDispatcherRule

--- pipe/src/test/java/com/udeyrishi/pipe/testutil/TestDispatcherRule.kt
+++ pipe/src/test/java/com/udeyrishi/pipe/testutil/TestDispatcherRule.kt
@@ -6,12 +6,11 @@ package com.udeyrishi.pipe.testutil
 import com.udeyrishi.pipe.PipelineDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import org.junit.Assert.fail
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
-class TestDispatcherRule(private val errorExpected: Boolean = false) : TestRule {
+class TestDispatcherRule : TestRule {
     lateinit var dispatcher: PipelineDispatcher
         private set
 
@@ -25,7 +24,7 @@ class TestDispatcherRule(private val errorExpected: Boolean = false) : TestRule 
                 try {
                     base.evaluate()
                 } finally {
-                    dispatcher.verify(errorExpected)
+                    dispatcher.verify()
                 }
             }
         }
@@ -41,27 +40,9 @@ private class DefaultTestDispatcher : PipelineDispatcher {
         lastUnhandledError = throwable
     }
 
-    fun verify(errorExpected: Boolean) {
-        if (errorExpected) {
-            var attempt = 0
-            while (lastUnhandledError == null && attempt++ < MAX_VERIFY_ATTEMPTS) {
-                Thread.sleep(POLL_DELAY_MS)
-            }
-
-            if (lastUnhandledError == null && attempt == MAX_VERIFY_ATTEMPTS) {
-                fail("Expected an error to be thrown, but none was received for ${POLL_DELAY_MS * MAX_VERIFY_ATTEMPTS} milliseconds.")
-            }
-        }
-
-        val errorToBeThrown = lastUnhandledError
-        lastUnhandledError = null
-        errorToBeThrown?.let {
+    fun verify() {
+        lastUnhandledError?.let {
             throw it
         }
-    }
-
-    companion object {
-        private const val MAX_VERIFY_ATTEMPTS = 1000
-        private const val POLL_DELAY_MS = 10L
     }
 }
