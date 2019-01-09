901958126549f29dca648a4dc2259ed26351bc44
Udey Rishi <udey.rishi@gmail.com>
2019-01-08 23:43:52 -0500

    Added docs for lift extensions

--- pipe/src/main/java/com/udeyrishi/pipe/util/BarrierExtensions.kt
+++ pipe/src/main/java/com/udeyrishi/pipe/util/BarrierExtensions.kt
@@ -17,10 +17,18 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
+/**
+ * Represents an operation that can be cancelled.
+ */
 interface Cancellable {
     fun cancel()
 }
 
+/**
+ * A utility extension function that can be used to automatically lift a [ManualBarrierController] when
+ * a certain [condition] is a met. The [condition] will be evaluated every [periodicityMillis] milliseconds
+ * on a coroutine allowed by the [pollingDispatcher].
+ */
 fun ManualBarrierController.liftWhen(periodicityMillis: Long = 500L, pollingDispatcher: PipelineDispatcher = Dispatchers.IO.toStrictAndroidPipeDispatcher(), condition: suspend () -> Boolean): Cancellable {
     val job = GlobalScope.launch(pollingDispatcher.createEffectiveContext()) {
         while (!condition() && isActive) {
@@ -37,6 +45,10 @@ fun ManualBarrierController.liftWhen(periodicityMillis: Long = 500L, pollingDisp
     }
 }
 
+/**
+ * A utility extension function that be used to lift a barrier when internet is available. It can be
+ * used in front of the steps that require internet access.
+ */
 @RequiresPermission(ACCESS_NETWORK_STATE)
 fun ManualBarrierController.liftWhenHasInternet(app: Application) {
     val connectivityMonitor = app.createConnectivityMonitor()
